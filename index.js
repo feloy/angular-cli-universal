1862c57977d8ffc21f10c401cf4fa44a144376c9
pmartin <contact@elol.fr>
2017-07-23 08:38:02 +0200

    With AOT compilation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('zone.js/dist/zone-node');
 require('reflect-metadata');
 const fs = require('fs');
-const { AppModule } = require('./dist/main.bundle');
-const { renderModule } = require('@angular/platform-server');
-renderModule(AppModule, {
+const { AppModuleNgFactory } = require('./dist/main.bundle');
+const { renderModuleFactory } = require('@angular/platform-server');
+renderModuleFactory(AppModuleNgFactory, {
     url: '/',
     document: '<app-root></app-root>'
 }).then(html => {
